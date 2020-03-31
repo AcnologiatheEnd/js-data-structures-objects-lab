c7e6a4053d5ac53d7d43e83bf2cd1d40f8568785
Winston  Chung <18.chung.winston@ctkprep.org>
2020-03-31 23:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ function updateDriverWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+}
+
+function deleteFromDriverByKey(obj, key) {
   const newObj = { ...obj }
   
   delete newObj.key
  
   return newObj;
-}
-
-function deleteFromDriverByKey(obj, key) {
-  delete obj.key
 }
 
 function destructivelyDeleteFromDriverByKey(obj, key) {
